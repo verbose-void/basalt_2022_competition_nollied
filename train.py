bd07c7a7ed63a58ff049a1c729e169a518cedfa1
nollied <dyllanmccreary@gmail.com>
2022-10-08 21:35:49 -0700

    begin train script + playground nb

--- train.py
+++ train.py
@@ -21,6 +21,34 @@ VPT_MODELS_ROOT = os.path.join(MINERL_DATA_ROOT, "VPT-models/")
 PRETRAINED_AGENT_MODEL_FILE = os.path.join(VPT_MODELS_ROOT, "foundation-model-2x.model")
 PRETRAINED_AGENT_WEIGHTS_FILE = os.path.join(VPT_MODELS_ROOT, "rl-from-early-game-2x.weights")
 
+
+NUM_WALKERS = 8
+DISCRIMINATOR_CLASSES = 2  # TODO: set discriminator classes to num of tasks
+TARGET_LOGIT = 0
+UNROLL_STEPS = 8
+
+
+def get_agent():
+    return load_agent(PRETRAINED_AGENT_MODEL_FILE, PRETRAINED_AGENT_WEIGHTS_FILE)
+
+
+def get_dynamics_function():
+    return DynamicsFunction(
+        discriminator_classes=DISCRIMINATOR_CLASSES,
+    )
+
+
+def get_dynamics_environment(minerl_env: gym.Env) -> MineRLDynamicsEnvironment:
+    dynamics_function = get_dynamics_function()
+    return MineRLDynamicsEnvironment(
+        minerl_env.action_space, 
+        dynamics_function=dynamics_function, 
+        target_discriminator_logit=TARGET_LOGIT,
+        n=NUM_WALKERS,
+    )
+
+
+
 def main():
     """
     This function will be called for training phase.
@@ -29,29 +57,10 @@ def main():
     """
 
     minerl_env = gym.make('MineRLBasaltFindCave-v0')
-
-    NUM_WALKERS = 8
-    DISCRIMINATOR_CLASSES = 2  # TODO: set discriminator classes to num of tasks
-    TARGET_LOGIT = 0
-    UNROLL_STEPS = 8
-
-    agent = load_agent(PRETRAINED_AGENT_MODEL_FILE, PRETRAINED_AGENT_WEIGHTS_FILE)
-
-    dynamics_function = DynamicsFunction(
-        discriminator_classes=DISCRIMINATOR_CLASSES,
-    )
-
-    dynamics_env = MineRLDynamicsEnvironment(
-        minerl_env.action_space, 
-        dynamics_function=dynamics_function, 
-        target_discriminator_logit=TARGET_LOGIT,
-        n=NUM_WALKERS,
-    )
-
+    agent = get_agent()
+    dynamics_env = get_dynamics_environment(minerl_env)
     fmc = FMC(dynamics_env)
-
     trainer = FGZTrainer(minerl_env, agent, fmc, unroll_steps=UNROLL_STEPS)
-
 
     # For an example, lets just run 100 steps of the environment for training
     # obs = env.reset()
