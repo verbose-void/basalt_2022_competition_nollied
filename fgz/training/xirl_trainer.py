e8d346bd13667923627326ee820f51a8f88b0bc0
McCrearyD <mccreary@dyllan.ai>
2022-10-17 11:27:57 -0700

    wrote soft nearest neighbor

--- fgz/training/xirl_trainer.py
+++ fgz/training/xirl_trainer.py
@@ -1,10 +1,14 @@
 
+from re import I
+from typing import List
 import ray
 from vpt.agent import MineRLAgent
 from fgz.architecture.dynamics_function import DynamicsFunction
 
 from fgz.data_utils.xirl_data import XIRLDataHandler
 from vpt.run_agent import load_agent
+
+import torch
 
 
 # @ray.remote
@@ -18,9 +22,27 @@ class XIRLTrainer:
 
         self.data_handler = XIRLDataHandler(dataset_path, self.agent, self.dynamics_function)
 
+    def soft_nearest_neighbor(self, frame_embedding: torch.Tensor, other_embeddings: List[torch.Tensor]):
+        # TODO: optimize this!
+        soft_v = 0
+
+        for vj in other_embeddings:
+            # first, we calculate the divisor of equation 1.
+            divisor = 0
+            for vk in other_embeddings:
+                divisor += torch.exp(-torch.norm(frame_embedding - vk))
+            numerator = torch.exp(-torch.norm(frame_embedding - vj))
+            alpha_j = numerator / divisor
+            soft_v += alpha_j * vj
+        
+        return soft_v
+
     def train_on_pair(self):
         self.t0, self.t1 = self.data_handler.sample_pair()
         print("bytes for the pair of trajectories:", self.get_nbytes_stored())
+
+        for embedding0 in self.t0:
+            pass
 
         # TODO: cycle consistency loss + train
 
