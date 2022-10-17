698a0155720fe71e85803b384690991b69c2ec79
McCrearyD <mccreary@dyllan.ai>
2022-10-17 11:53:20 -0700

    calculate both similarity measures

--- fgz/training/xirl_trainer.py
+++ fgz/training/xirl_trainer.py
@@ -22,27 +22,43 @@ class XIRLTrainer:
 
         self.data_handler = XIRLDataHandler(dataset_path, self.agent, self.dynamics_function)
 
-    def soft_nearest_neighbor(self, frame_embedding: torch.Tensor, other_embeddings: List[torch.Tensor]):
+    def soft_nearest_neighbor(self, frame_embedding: torch.Tensor, other_embeddings: List[torch.Tensor], return_similarity_vector: bool):
         # TODO: optimize this!
         soft_v = 0
 
-        for vj in other_embeddings:
+        similarity_vector = torch.zeros(size=(len(other_embeddings),), dtype=float)
+
+        for j, vj in enumerate(other_embeddings):
             # first, we calculate the divisor of equation 1.
             divisor = 0
             for vk in other_embeddings:
                 divisor += torch.exp(-torch.norm(frame_embedding - vk))
             numerator = torch.exp(-torch.norm(frame_embedding - vj))
             alpha_j = numerator / divisor
-            soft_v += alpha_j * vj
+
+            if return_similarity_vector:
+                similarity_vector[j] = alpha_j
+            else:
+                soft_v += alpha_j * vj
         
+        if return_similarity_vector:
+            return similarity_vector
         return soft_v
 
-    def train_on_pair(self):
+    def train_on_pair(self, num_frames: int=20):
         self.t0, self.t1 = self.data_handler.sample_pair()
         print("bytes for the pair of trajectories:", self.get_nbytes_stored())
 
-        for embedding0 in self.t0:
-            pass
+        for _ in range(num_frames):
+            # pick random frame in t0
+            i = torch.randint(low=0, high=len(self.t0), size=(1,)).item()
+            ui = self.t0[i]
+
+            # calculate cycle MSE loss
+            v_squiggly = self.soft_nearest_neighbor(ui, self.t1, return_similarity_vector=False)
+            beta = self.soft_nearest_neighbor(v_squiggly, self.t0, return_similarity_vector=True)
+
+            print(beta)
 
         # TODO: cycle consistency loss + train
 
