ac57389ec61c1c2d7d8541d613eb9957dc3f2ecb
nollied <dyllanmccreary@gmail.com>
2022-10-21 15:27:54 -0700

    arg parser for play checkpoint

--- play_checkpoint.py
+++ play_checkpoint.py
@@ -1,7 +1,14 @@
+import argparse
 from fgz.training.fgz_trainer import FGZTrainer
 from train import get_agent
 
-trainer = FGZTrainer.load("train/fgz_dynamics_checkpoint.pth")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path")
+
+args = parser.parse_args().__dict__
+
+trainer = FGZTrainer.load(**args)
 
 trainer.evaluate(
     "MineRLBasaltMakeWaterfall-v0", render=True, max_steps=256, force_no_escape=True
