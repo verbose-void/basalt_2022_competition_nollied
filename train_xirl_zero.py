3079053b535479308e20954e3ff0c3b538e2420d
nollied <dyllanmccreary@gmail.com>
2022-10-27 20:22:08 -0700

    smoke test tcc loss

--- train_xirl_zero.py
+++ train_xirl_zero.py
@@ -4,7 +4,9 @@ from xirl_zero.main_trainer import Config, Trainer
 
 
 if __name__ == "__main__":
-    dataset_path = "/Volumes/CORSAIR/data/MineRLBasaltMakeWaterfall-v0"  # TODO: determine based on task idx
+    # TODO: determine based on task idx
+    # dataset_path = "/Volumes/CORSAIR/data/MineRLBasaltMakeWaterfall-v0"
+    dataset_path = "./data/MineRLBasaltMakeWaterfall-v0"
 
     config = Config(dataset_path=dataset_path)
     trainer = Trainer(config)
--- xirl_zero/trainers/tcc_representation.py
+++ xirl_zero/trainers/tcc_representation.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import List, Tuple
+
 import torch
+import torch.nn.functional as F
 
 import os
 
@@ -22,7 +24,10 @@ class TCCConfig:
 
     learning_rate: float = 0.001
 
-    embed_batch_size: int = 32
+    temperature: float = 0.1
+
+    batch_size: int = 32        # gradients
+    embed_batch_size: int = 32  # no gradients
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -73,12 +78,78 @@ class TCCRepresentationTrainer:
 
         return embedded.float()
 
+    def calculate_tcc_loss(
+        self, 
+        embedded_t0: torch.Tensor, 
+        embedded_t1: torch.Tensor, 
+        chosen_frame_indices: torch.Tensor,
+        embedded_chosen_frames: torch.Tensor,    
+    ):
+
+        # TODO: vectorize better
+
+        bs = self.config.batch_size
+        assert len(chosen_frame_indices) == len(embedded_chosen_frames) == bs
+
+        max_index = len(embedded_t0)
+
+        index_preds = []
+        index_logits = []
+
+        for i in range(bs):
+            ui = embedded_chosen_frames[i]
+
+            # calculate cycle MSE loss
+            alpha_k = self.soft_nearest_neighbor(
+                ui, embedded_t1
+            )
+            v_squiggly = torch.sum(alpha_k.unsqueeze(-1) * embedded_t1, dim=0)
+
+            beta = self.soft_nearest_neighbor(
+                v_squiggly, embedded_t0
+            )
+
+            frame_mult = torch.arange(
+                start=1, end=len(beta) + 1, dtype=float, device=beta.device
+            ).float()# / len(beta)
+            mu = torch.matmul(frame_mult, beta) # / len(beta)
+
+            index_preds.append(mu)
+            index_logits.append(beta)
+
+        index_preds = torch.stack(index_preds)
+
+        # mse version
+        # unnormalized_mse = F.mse_loss(index_preds, chosen_frame_indices)
+        normalized_mse = F.mse_loss(index_preds / max_index, chosen_frame_indices / max_index)
+
+        # cross entropy version
+        # index_logits = torch.stack(index_logits)
+        # cross_entropy_labels = chosen_frame_indices.long()
+        # ce_loss = F.cross_entropy(index_logits, cross_entropy_labels) / torch.log(torch.tensor(max_index))
+
+        return normalized_mse
+
+    def choose_random_frames(self, max_index: int):
+        return torch.randint(
+            low=0, high=max_index, size=(self.config.batch_size,)
+        )
+
     def train_step(self, t0: torch.Tensor, t1: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         """The representation function is trained using Temporal Cycle-Consistency (https://arxiv.org/pdf/1904.07846.pdf) 
         loss over a subset of the frames in a pair of demonstration trajectories.
         """
 
-        embedded_t0 = self.embed_trajectory(t0)
-        embedded_t1 = self.embed_trajectory(t1)
+        with torch.no_grad():
+            embedded_t0 = self.embed_trajectory(t0)
+            embedded_t1 = self.embed_trajectory(t1)
+
+        chosen_frame_indices = self.choose_random_frames(len(embedded_t0))
+
+        # embed chosen frames again, but this time with gradients
+        embedded_chosen_frames = self.model.embed(t0[chosen_frame_indices].to(self.config.device))
+
+        loss = self.calculate_tcc_loss(embedded_t0, embedded_t1, chosen_frame_indices.to(self.config.device), embedded_chosen_frames)
+        print(loss)
 
         return embedded_t0, embedded_t1
